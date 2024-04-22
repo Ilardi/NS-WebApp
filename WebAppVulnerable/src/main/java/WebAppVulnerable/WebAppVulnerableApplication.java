cd1bd1955468936f73b9651fbebc31c5bce65c31
Ilardi <luci.ilardi@studenti.unina.it>
2024-04-22 22:35:17 +0200

    Vulnerable app started

---
+++ WebAppVulnerable/src/main/java/WebAppVulnerable/WebAppVulnerableApplication.java
@@ -0,0 +1,13 @@
+package WebAppVulnerable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppVulnerableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppVulnerableApplication.class, args);
+	}
+
+}
---
+++ WebAppVulnerable/src/test/java/WebAppVulnerable/WebAppVulnerableApplicationTests.java
@@ -0,0 +1,13 @@
+package WebAppVulnerable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppVulnerableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
