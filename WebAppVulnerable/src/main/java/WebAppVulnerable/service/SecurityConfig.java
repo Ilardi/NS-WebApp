4e8d8635384839f9f070c561d6731e372afd28d1
Ilardi <luci.ilardi@studenti.unina.it>
2024-04-27 09:59:39 +0200

    CORS no longer needed

--- WebAppVulnerable/src/main/java/WebAppVulnerable/service/SecurityConfig.java
+++ WebAppVulnerable/src/main/java/WebAppVulnerable/service/SecurityConfig.java
@@ -1,20 +1,14 @@
 package WebAppVulnerable.service;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
@@ -29,28 +23,12 @@ public class SecurityConfig {
 	public PasswordEncoder encoder() {
 	    return new BCryptPasswordEncoder();
 	}
-    
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
- 
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", 
-        "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
-        
-        //Applica la configurazione a tutti i path
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 		
 		http.authorizeHttpRequests(t -> t
-				.requestMatchers("/home","/review").permitAll()
+				.requestMatchers("/home").permitAll()
 				.anyRequest().authenticated());
 		
 		http.formLogin(t -> t
@@ -58,9 +36,6 @@ public class SecurityConfig {
 		
 		http.logout(t -> t
 				.logoutSuccessUrl("/home"));
-		
-		//Abilita CORS (necessario per i link da sito evil)
-		http.cors(Customizer.withDefaults());
 		
 		//Disabilita CSRF protection
 		http.csrf(t -> t.disable());
